e758d8197e03a79b912c6cfe5282d282d6e72142
OanaOnici <OanaOnici@DESKTOP-N5DQF4E.local>
2020-03-15 23:30:22 +0200

    Primul commit

---
+++ src/TestGit.java
@@ -0,0 +1,13 @@
+
+public class TestGit
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello Git! Denumirea proiectului meu este Chatbot for flights");
+        
+       
+
+    }
+
+}
